163666d997dbf7fcb6573a16a7a0282de6c74027
Opar710 <giannidaino@yahoo.com>
2025-03-30 22:51:22 -0500

    cripts.js

---
+++ Chapter3Project/scripts.js
@@ -0,0 +1,23 @@
+// Name: Gianni | Date: March 30, 2025
+
+const fullName = "Gianni Example";
+console.log("Full Name:", fullName);
+
+const desiredSalary = 85000;
+console.log("Desired Salary:", desiredSalary);
+
+const isVeteran = false;
+console.log("Veteran Status:", isVeteran);
+
+const friends = ["Alex", "Jordan", "Casey"];
+console.log("Friends:", friends);
+
+const friendsSalaries = [70000, 60000, 90000];
+console.log("Friends' Salaries:", friendsSalaries);
+
+const anotherFriend = {
+  firstName: "Sam",
+  lastName: "Lee",
+  desiredSalary: 75000
+};
+console.log("Another Friend:", anotherFriend);
